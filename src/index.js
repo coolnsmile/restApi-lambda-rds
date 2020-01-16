453d0af7bf81cbb4b1e95325d3f72775e2023440
EC2 Default User <ec2-user@ip-10-0-1-143.ap-northeast-2.compute.internal>
2020-01-16 11:58:01 +0000

    url 변경 --> /search에서 /으로

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const port = 80;
 
-app.get('/search', function(req, res) {
+app.get('/', function(req, res) {
     let query = req.query.queryStr;
     let url = `https://1i6h45wdb5.execute-api.ap-northeast-2.amazonaws.com/test/kilhan`;
 
